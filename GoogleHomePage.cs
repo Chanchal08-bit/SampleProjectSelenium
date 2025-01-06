bdc94cfea2170ed1882412faac2698c491c9061f
Chanchal08-bit <verma.chanchal0811@gmail.com>
2025-01-06 11:53:39 +0530

    Add files via upload

---
+++ GoogleHomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCshrapProgram.Pages
+{
+    class GoogleHomePage
+    {
+
+        public static string txt_GoogleSearch = "//div/input[@title='Search']";
+        public static string btn_GoogleSearch = "//div[@class='VlcLAe']//input[@value='Google Search']";
+    }
+}
---
+++ GoogleSearchPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCshrapProgram.Pages
+{
+    class GoogleSearchPage
+    {
+        public static string txt_Avivalinks = "//h3[starts-with(text(),'Aviva')]";
+    }
+}
